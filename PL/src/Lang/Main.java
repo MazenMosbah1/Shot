3c7c4d72a765b747125d2d91f34b73cbb05a48f6
MazenMosbah1 <mazenmosbah5@icloud.com>
2023-05-11 15:24:46 +0300

    Second Release

---
+++ PL/src/Lang/Main.java
@@ -0,0 +1,14 @@
+package Lang;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        shot l = new shot();
+
+        shot.runFile("D:\\Shot\\PL\\src\\Tools\\Documentation\\IF_Statements");
+        //l.runPrompt();
+
+    }
+}
---
+++ PL/src/Tools/Main.java
@@ -0,0 +1,15 @@
+package Tools;
+
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        GenerateAst test = new GenerateAst();
+
+        test.main(Arrays.asList("E:\\Coding\\Programming-Lang\\PL\\src\\Tools").toArray(new String[0]));
+
+    }
+}
